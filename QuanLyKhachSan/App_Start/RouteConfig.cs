101a9a343be216294e2e08d66d7ef900c4c4edb1
linhfishCR7 <59117535+linhfishCR7@users.noreply.github.com>
2020-12-20 22:16:36 +0700

    first commit with user

---
+++ QuanLyKhachSan/App_Start/RouteConfig.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.FriendlyUrls;
+using Microsoft.AspNet.FriendlyUrls.Resolvers;
+
+namespace QuanLyKhachSan
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            var settings = new FriendlyUrlSettings();
+            settings.AutoRedirectMode = RedirectMode.Permanent;
+            routes.EnableFriendlyUrls(settings,
+                                        new IFriendlyUrlResolver[] {
+                                 new MyWebFormsFriendlyUrlResolver() });
+
+        }
+        public class MyWebFormsFriendlyUrlResolver : WebFormsFriendlyUrlResolver
+        {
+            public MyWebFormsFriendlyUrlResolver() { }
+
+            public override string ConvertToFriendlyUrl(string path)
+            {
+                if (!string.IsNullOrEmpty(path))
+                {
+                    if (path.ToLower().Contains("/ckfinder/"))
+                    { // Here the filter code
+                        return path;
+                    }
+                }
+                return base.ConvertToFriendlyUrl(path);
+            }
+        }
+    }
+}
