21abe4eb9a0d0cd8da751a8e75c806a9b9db14cb
linhfishCR7 <59117535+linhfishCR7@users.noreply.github.com>
2020-12-22 10:29:42 +0700

    Service,Class,Application

---
+++ QuanLyKhachSan/Admin/LoaiPhong.aspx.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QuanLyKhachSan.Admin
+{
+    public partial class LoaiPhong : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Add Modal
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(@"<script type='text/javascript'>");
+            sb.Append("$('#addModal').modal('show');");
+            sb.Append(@"</script>");
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "AddShowModalScript", sb.ToString(), false);
+
+        }
+
+
+
+        //([LP_MA], [LP_GIA],[LP_SONGUOIMAX])
+        protected void btnAddRecord_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                var parameters = SqlDataSource1.InsertParameters;
+
+                parameters["LP_MA"].DefaultValue = txtLP_MA.Text;
+                parameters["LP_GIA"].DefaultValue = txtLP_GIA.Text;
+                parameters["LP_SONGUOIMAX"].DefaultValue = txtLP_SONGUOIMAX.Text;
+
+                try
+                {
+                    if (txtLP_MA.Text == "")
+                    {
+                        ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Các trường (*) không được bỏ trống.');", true);
+                    }
+                    else if (txtLP_GIA.Text == "")
+                    {
+                        ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Các trường (*) không được bỏ trống.');", true);
+                    }
+                    else if (txtLP_SONGUOIMAX.Text == "")
+                    {
+                        ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Các trường (*) không được bỏ trống.');", true);
+                    }
+
+
+                    else
+                    {
+                        SqlDataSource1.Insert();
+                        txtLP_MA.Text = "";
+                        txtLP_GIA.Text = "";
+                        txtLP_SONGUOIMAX.Text = "";
+
+
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append(@"<script type='text/javascript'>");
+                        sb.Append("alert('Record Added Successfully');");
+                        sb.Append("$('#addModal').modal('hide');");
+                        sb.Append(@"</script>");
+                        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "AddHideModalScript", sb.ToString(), false);
+
+                    }
+                }
+                catch (Exception)
+                {
+                    ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Vui lòng kiểm tra lại.');", true);
+                }
+            }
+        }
+
+
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int index = Convert.ToInt32(e.CommandArgument);
+            if (e.CommandName.Equals("editRecord"))
+            {
+                GridViewRow gvrow = GridView1.Rows[index];
+                txtLP_MA1.Text = HttpUtility.HtmlDecode(gvrow.Cells[2].Text).ToString();
+                txtLP_GIA1.Text = HttpUtility.HtmlDecode(gvrow.Cells[3].Text).ToString();
+                txtLP_SONGUOIMAX1.Text = HttpUtility.HtmlDecode(gvrow.Cells[4].Text).ToString();
+
+
+                lblResult.Visible = false;
+                StringBuilder sb = new StringBuilder();
+                sb.Append(@"<script type='text/javascript'>");
+                sb.Append("$('#editModal').modal('show');");
+                sb.Append(@"</script>");
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "EditModalScript", sb.ToString(), false);
+
+            }
+            else if (e.CommandName.Equals("deleteRecord"))
+            {
+                string code = GridView1.DataKeys[index].Value.ToString();
+                hfCode.Value = code;
+                StringBuilder sb = new StringBuilder();
+                sb.Append(@"<script type='text/javascript'>");
+                sb.Append("$('#deleteModal').modal('show');");
+                sb.Append(@"</script>");
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "DeleteModalScript", sb.ToString(), false);
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                var parameters = SqlDataSource1.UpdateParameters;
+
+                //([LP_MA], [LP_TEN],[LP_SONGUOIMAX])                
+                parameters["LP_MA"].DefaultValue = txtLP_MA1.Text;
+                parameters["LP_GIA"].DefaultValue = txtLP_GIA1.Text;
+                parameters["LP_SONGUOIMAX"].DefaultValue = txtLP_SONGUOIMAX1.Text;
+
+                try
+                {
+                    if (txtLP_MA1.Text == "")
+                    {
+                        ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Các trường (*) không được bỏ trống.');", true);
+                    }
+                    else if (txtLP_GIA1.Text == "")
+                    {
+                        ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Các trường (*) không được bỏ trống.');", true);
+                    }
+                    else if (txtLP_SONGUOIMAX1.Text == "")
+                    {
+                        ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Các trường (*) không được bỏ trống.');", true);
+                    }
+
+                    else
+                    {
+                        SqlDataSource1.Update();
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append(@"<script type='text/javascript'>");
+                        sb.Append("alert('Records Updated Successfully');");
+                        sb.Append("$('#editModal').modal('hide');");
+                        sb.Append(@"</script>");
+                        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "EditHideModalScript", sb.ToString(), false);
+                    }
+                }
+                catch (Exception)
+                {
+                    ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Vui lòng kiểm tra lại.');", true);
+                }
+            }
+
+        }
+
+        private void executeDelete(string code)
+        {
+            if (IsValid)
+            {
+                var parameters = SqlDataSource1.DeleteParameters;
+                parameters["LP_MA"].DefaultValue = code;
+
+                try
+                {
+                    SqlDataSource1.Delete();
+                }
+                catch (Exception)
+                {
+                    ScriptManager.RegisterStartupScript(this, GetType(), "showalert", "alert('Vui lòng kiểm tra lại.');", true);
+                }
+            }
+
+        }
+
+        //Handles Delete button click in delete modal popup
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            string code = hfCode.Value;
+            executeDelete(code);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(@"<script type='text/javascript'>");
+            sb.Append("alert('Record deleted Successfully');");
+            sb.Append("$('#deleteModal').modal('hide');");
+            sb.Append(@"</script>");
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "delHideModalScript", sb.ToString(), false);
+        }
+    }
+}
